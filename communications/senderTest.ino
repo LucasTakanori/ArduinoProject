690d78876217ec02d2c1aea2cf6c52aff25a3eff
LucasTakanori <79314835+LucasTakanori@users.noreply.github.com>
2021-08-01 12:35:06 +0200

    Update senderTest.ino

--- communications/senderTest.ino
+++ communications/senderTest.ino
@@ -9,15 +9,24 @@ SoftwareSerial HC12(10, 11); // HC-12 TX Pin, HC-12 RX Pin
 void setup() {
   Serial.begin(9600);     //Initialize serial communication
   HC12.begin(9600);     //Initialize HC12 Transmiter
+if (HC12.isListening()) {
+    Serial.println("OK");
+  } else {
+    Serial.println("NOK");
+  }
   
   
 }
 
 void loop(){   
    
-  HC12.println("hola sergio!!!");   //
-  Serial.println("{\"humidity\":66,\"temperature\":26.5,\"floorhumidity0\":351,\"floorhumidity1\":352,\"floorhumidity2\":343,\"fHPercentages0\":81,\"fHPercentages1\":82,\"fHPercentages2\":88}");
-  HC12.println("{\"humidity\":66,\"temperature\":26.5,\"floorhumidity0\":351,\"floorhumidity1\":352,\"floorhumidity2\":343,\"fHPercentages0\":81,\"fHPercentages1\":82,\"fHPercentages2\":88}");
+  //HC12.println("hola sergio!!!");   //
+  StaticJsonDocument<200> doc;
+  doc["hello"] = "world";
+  serializeJson(doc, Serial);
+  serializeJson(doc, HC12);
+  //Serial.println("{\"humidity\":66,\"temperature\":26.5,\"floorhumidity0\":351,\"floorhumidity1\":352,\"floorhumidity2\":343,\"fHPercentages0\":81,\"fHPercentages1\":82,\"fHPercentages2\":88}");
+  //HC12.println("{\"humidity\":66,\"temperature\":26.5,\"floorhumidity0\":351,\"floorhumidity1\":352,\"floorhumidity2\":343,\"fHPercentages0\":81,\"fHPercentages1\":82,\"fHPercentages2\":88}");
  
   delay(2000);  
   
